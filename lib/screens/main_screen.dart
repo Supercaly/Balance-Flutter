d465ff54d6b622aaa03f62c246cb21548c586a96
supercaly <djonline96@gmail.com>
2020-03-31 20:21:07 +0200

    Add structure for the custom bottom navigation bar

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -45,22 +45,131 @@ class _MainScreenState extends State<MainScreen> {
         }),
         child: _pages[_currentIndex]
       ),
-      bottomNavigationBar: BottomNavigationBar(
+      bottomNavigationBar: GoogleBottomNavigationBar(
         currentIndex: _currentIndex,
-        selectedItemColor: BColors.colorPrimary,
-        showSelectedLabels: false,
-        showUnselectedLabels: false,
-        onTap: (newIdx) => setState(() {
-          // Set the current page to the selected and update the current index
-          //_pageController.jumpToPage(newIdx);
-          setState(() => _currentIndex = newIdx);
-        }),
         items: [
-          BottomNavigationBarItem(title: Text("Home"), icon: Icon(Icons.home)),
-          BottomNavigationBarItem(title: Text("Measurements"), icon: Icon(Icons.near_me)),
-          BottomNavigationBarItem(title: Text("Calibrate Device"), icon: Icon(Icons.calendar_today)),
+          NavigationItem(icon: Icon(Icons.home), text: Text("Home")),
+          NavigationItem(icon: Icon(Icons.list), text: Text("Tests")),
+          NavigationItem(icon: Icon(Icons.settings), text: Text("Settings")),
         ],
+        onTap: (newIdx) => setState(() => _currentIndex = newIdx),
       ),
     );
   }
+}
+
+class GoogleBottomNavigationBar extends StatefulWidget {
+  final Color backgroundColor;
+  final Color selectedColor;
+  final Color unselectedColor;
+  final Color rectangleColor;
+  final List<NavigationItem> items;
+  final int currentIndex;
+  final ValueChanged<int> onTap;
+
+  GoogleBottomNavigationBar({
+    Key key,
+    @required this.items,
+    this.onTap,
+    this.currentIndex = 0,
+    this.backgroundColor = Colors.white,
+    this.selectedColor = BColors.colorPrimary,
+    this.unselectedColor = BColors.textColor,
+    this.rectangleColor = const Color(0xffe8e7ff),
+  }): assert(items != null),
+    assert(items.length >= 2),
+    assert(0 <= currentIndex && currentIndex < items.length),
+    super(key: key);
+
+  @override
+  _GoogleBottomNavigationBarState createState() => _GoogleBottomNavigationBarState();
+}
+
+class _GoogleBottomNavigationBarState extends State<GoogleBottomNavigationBar> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      height: 56,
+      padding: EdgeInsets.only(left: 24, top: 6, right: 24, bottom: 6),
+      decoration: BoxDecoration(
+        color: widget.backgroundColor,
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black12,
+            blurRadius: 5
+          )
+        ]
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: widget.items.map((item) {
+          final itemIdx = widget.items.indexOf(item);
+          return Material(
+            color: widget.backgroundColor,
+            child: InkWell(
+              customBorder: CircleBorder(),
+              onTap: () {
+                if (widget.currentIndex != itemIdx) {
+                  widget.onTap.call(itemIdx);
+                }
+              },
+              child: _buildItem(item, widget.currentIndex == itemIdx),
+            ),
+          );
+        }).toList()
+      )
+    );
+  }
+
+  Widget _buildItem(NavigationItem item, bool isSelected) {
+    return AnimatedContainer(
+      duration: Duration(milliseconds: 270),
+      width: isSelected ? 122 : 50,
+      height: double.maxFinite,
+      decoration: isSelected ? BoxDecoration(
+        color: widget.rectangleColor,
+        borderRadius: BorderRadius.circular(90)
+      ): null,
+      child: Center(
+        child: ListView(
+          scrollDirection: Axis.horizontal,
+          shrinkWrap: true,
+          children: <Widget>[
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                IconTheme(
+                  data: IconThemeData(
+                    size: 24,
+                    color: isSelected ? widget.selectedColor: widget.unselectedColor
+                  ),
+                  child: item.icon,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 12),
+                  child: isSelected ?
+                    DefaultTextStyle.merge(
+                      style: TextStyle(
+                        color: widget.selectedColor,
+                        fontSize: 12,
+                        fontWeight: FontWeight.w500
+                      ),
+                      child: item.text
+                    ): Container(),
+                )
+              ],
+            ),
+          ]
+        ),
+      )
+    );
+  }
+}
+
+class NavigationItem {
+  final Icon icon;
+  final Text text;
+
+  NavigationItem({this.icon, this.text});
 }
