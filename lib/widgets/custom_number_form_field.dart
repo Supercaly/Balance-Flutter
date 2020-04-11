2031530646015741f206843d5fd24b350cd01a81
supercaly <djonline96@gmail.com>
2020-04-11 18:46:58 +0200

    Change custom_number_form_field.dart style

--- lib/widgets/custom_number_form_field.dart
+++ lib/widgets/custom_number_form_field.dart
@@ -22,24 +22,40 @@ class CustomNumberFormField extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 4),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(9),
-        color: Colors.white,
-      ),
-      child: TextFormField(
-        keyboardType: TextInputType.numberWithOptions(decimal: decimal),
-        decoration: InputDecoration(
-          border: InputBorder.none,
-          labelText: labelText,
-          suffixText: suffix,
+    return Material(
+      elevation: 4,
+      color: Colors.white,
+      borderRadius: BorderRadius.circular(9),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 4),
+        child: TextFormField(
+          keyboardType: TextInputType.numberWithOptions(decimal: decimal),
+          decoration: InputDecoration(
+            border: InputBorder.none,
+            hintText: labelText,
+            suffixText: suffix,
+            hintStyle: TextStyle(
+              color: Color(0xFFBFBFBF),
+              fontSize: 14,
+              fontWeight: FontWeight.w500,
+            ),
+            suffixStyle: TextStyle(
+              color: Color(0xFFBFBFBF),
+              fontSize: 14,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+          style: TextStyle(
+            color: Color(0xFFBFBFBF),
+            fontSize: 14,
+            fontWeight: FontWeight.w500,
+          ),
+          autocorrect: false,
+          initialValue: initialValue,
+          onChanged: (newValue) => onChanged?.call(newValue.isNotEmpty),
+          validator: validator,
+          onSaved: onSaved,
         ),
-        autocorrect: false,
-        initialValue: initialValue,
-        onChanged: (newValue) => onChanged?.call(newValue.isNotEmpty),
-        validator: validator,
-        onSaved: onSaved,
       ),
     );
   }
