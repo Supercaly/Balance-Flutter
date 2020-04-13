de0747b13a7377e24cff5e17c13befc7be707b87
supercaly <djonline96@gmail.com>
2020-04-13 18:20:16 +0200

    Add bloc to personal_info_recap_screen.dart

--- lib/screens/personal_info_recap_screen.dart
+++ lib/screens/personal_info_recap_screen.dart
@@ -1,7 +1,7 @@
 
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
+import 'package:balance_app/bloc/user_info_bloc.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class PersonalInfoRecapScreen extends StatelessWidget {
   @override
@@ -17,224 +17,237 @@ class PersonalInfoRecapScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text("Your personal info"),
       ),
-      body: ListView(
-        children: <Widget>[
-          // General Info Card
-          Card(
-            margin: const EdgeInsets.only(left: 16, top: 16, right: 16, bottom: 8),
-            child: Padding(
-              padding: const EdgeInsets.all(16),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Text(
-                    "General".toUpperCase(),
-                    style: titleTextStyle,
+      body: BlocProvider<UserInfoBloc>(
+        create: (context) => UserInfoBloc.create(),
+        child: BlocBuilder<UserInfoBloc, UserInfoState>(
+          builder: (context, state) {
+            if (state is UserInfoLoading)
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            final userInfo = (state as UserInfoSuccess).value;
+            // TODO: 13/04/20 Add method to convert from UserInfo to strings
+            return ListView(
+              children: <Widget>[
+                // General Info Card
+                Card(
+                  margin: const EdgeInsets.only(left: 16, top: 16, right: 16, bottom: 8),
+                  child: Padding(
+                    padding: const EdgeInsets.all(16),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          "General".toUpperCase(),
+                          style: titleTextStyle,
+                        ),
+                        SizedBox(height: 16),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget>[
+                            Text(
+                              "Age",
+                              style: headlineTextStyle,
+                            ),
+                            Text(
+                              userInfo?.age.toString(),
+                              style: valueTextStyle,
+                            ),
+                          ],
+                        ),
+                        SizedBox(height: 16),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget>[
+                            Text(
+                              "Gender",
+                              style: headlineTextStyle,
+                            ),
+                            Text(
+                              userInfo?.gender == 0? "unknown": userInfo?.gender == 1? "male": "female",
+                              style: valueTextStyle,
+                            ),
+                          ],
+                        ),
+                        SizedBox(height: 16),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget>[
+                            Text(
+                              "Height",
+                              style: headlineTextStyle,
+                            ),
+                            Text(
+                              "${userInfo.height} cm",
+                              style: valueTextStyle,
+                            ),
+                          ],
+                        ),
+                        SizedBox(height: 16),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget>[
+                            Text(
+                              "Weight",
+                              style: headlineTextStyle,
+                            ),
+                            Text(
+                              "${userInfo.weight} Kg",
+                              style: valueTextStyle,
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
                   ),
-                  SizedBox(height: 16),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: <Widget>[
-                      Text(
-                        "Age",
-                        style: headlineTextStyle,
-                      ),
-                      Text(
-                        "18",
-                        style: valueTextStyle,
-                      ),
-                    ],
+                ),
+                // Health Info Card
+                Card(
+                  margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                  child: Padding(
+                    padding: const EdgeInsets.all(16),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          "Health".toUpperCase(),
+                          style: titleTextStyle,
+                        ),
+                        SizedBox(height: 16),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget>[
+                            Text(
+                              "Postural problems",
+                              style: headlineTextStyle,
+                            ),
+                            Text(
+                              "[${userInfo?.posturalProblems?.join(", ")}]",
+                              style: valueTextStyle,
+                              textAlign: TextAlign.end,
+                            ),
+                          ],
+                        ),
+                        SizedBox(height: 16),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget>[
+                            Text(
+                              "Postural problems in family",
+                              style: headlineTextStyle,
+                            ),
+                            Text(
+                              userInfo.problemsInFamily? "yes": "no",
+                              style: valueTextStyle,
+                            ),
+                          ],
+                        ),
+                        SizedBox(height: 16),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Flexible(
+                              child: Text(
+                                "Use of drugs that can interfere with balance",
+                                style: headlineTextStyle,
+                              )
+                            ),
+                            Text(
+                              userInfo.useOfDrugs? "yes": "no",
+                              style: valueTextStyle,
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
                   ),
-                  SizedBox(height: 16),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: <Widget>[
-                      Text(
-                        "Gender",
-                        style: headlineTextStyle,
-                      ),
-                      Text(
-                        "male",
-                        style: valueTextStyle,
-                      ),
-                    ],
+                ),
+                // Trauma Info Card
+                Card(
+                  margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                  child: Padding(
+                    padding: const EdgeInsets.all(16),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          "Trauma".toUpperCase(),
+                          style: titleTextStyle,
+                        ),
+                        SizedBox(height: 16),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text(
+                              "Other trauma",
+                              style: headlineTextStyle,
+                            ),
+                            SizedBox(width: 16),
+                            Flexible(
+                              child: Text(
+                                "[${userInfo?.otherTrauma?.join(", ")}]",
+                                style: valueTextStyle,
+                                textAlign: TextAlign.end,
+                              )
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
                   ),
-                  SizedBox(height: 16),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: <Widget>[
-                      Text(
-                        "Height",
-                        style: headlineTextStyle,
-                      ),
-                      Text(
-                        "180 cm",
-                        style: valueTextStyle,
-                      ),
-                    ],
+                ),
+                // Sight/Hear Defects Info Card
+                Card(
+                  margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                  child: Padding(
+                    padding: const EdgeInsets.all(16),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          "Difetti Visivi/Uditivi".toUpperCase(),
+                          style: titleTextStyle,
+                        ),
+                        SizedBox(height: 16),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget>[
+                            Text(
+                              "Difetti visivi",
+                              style: headlineTextStyle,
+                            ),
+                            Text(
+                              "${userInfo?.sightProblems?.join(", ")}",
+                              style: valueTextStyle,
+                              textAlign: TextAlign.end,
+                            ),
+                          ],
+                        ),
+                        SizedBox(height: 16),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget>[
+                            Text(
+                              "Difetti uditivi",
+                              style: headlineTextStyle,
+                            ),
+                            Text(
+                              userInfo.hearingProblems.toString(),
+                              style: valueTextStyle,
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
                   ),
-                  SizedBox(height: 16),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: <Widget>[
-                      Text(
-                        "Weight",
-                        style: headlineTextStyle,
-                      ),
-                      Text(
-                        "80 Kg",
-                        style: valueTextStyle,
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ),
-          ),
-          // Health Info Card
-          Card(
-            margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-            child: Padding(
-              padding: const EdgeInsets.all(16),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Text(
-                    "Health".toUpperCase(),
-                    style: titleTextStyle,
-                  ),
-                  SizedBox(height: 16),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: <Widget>[
-                      Text(
-                        "Postural problems",
-                        style: headlineTextStyle,
-                      ),
-                      Text(
-                        "[scogliosi, cifosi]",
-                        style: valueTextStyle,
-                        textAlign: TextAlign.end,
-                      ),
-                    ],
-                  ),
-                  SizedBox(height: 16),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: <Widget>[
-                      Text(
-                        "Postural problems in family",
-                        style: headlineTextStyle,
-                      ),
-                      Text(
-                        "no",
-                        style: valueTextStyle,
-                      ),
-                    ],
-                  ),
-                  SizedBox(height: 16),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Flexible(
-                        child: Text(
-                          "Use of drugs that can interfere with balance",
-                          style: headlineTextStyle,
-                        )
-                      ),
-                      Text(
-                        "yes",
-                        style: valueTextStyle,
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ),
-          ),
-          // Trauma Info Card
-          Card(
-            margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-            child: Padding(
-              padding: const EdgeInsets.all(16),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Text(
-                    "Trauma".toUpperCase(),
-                    style: titleTextStyle,
-                  ),
-                  SizedBox(height: 16),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text(
-                        "Other trauma",
-                        style: headlineTextStyle,
-                      ),
-                      SizedBox(width: 16),
-                      Flexible(
-                        child: Text(
-                          "[Fratture, Operazioni agli arti, Cadute, Distorsioni, Trauma Cranico]",
-                          style: valueTextStyle,
-                          textAlign: TextAlign.end,
-                        )
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ),
-          ),
-          // Sight/Hear Defects Info Card
-          Card(
-            margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-            child: Padding(
-              padding: const EdgeInsets.all(16),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Text(
-                    "Difetti Visivi/Uditivi".toUpperCase(),
-                    style: titleTextStyle,
-                  ),
-                  SizedBox(height: 16),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: <Widget>[
-                      Text(
-                        "Difetti visivi",
-                        style: headlineTextStyle,
-                      ),
-                      Text(
-                        "none",
-                        style: valueTextStyle,
-                        textAlign: TextAlign.end,
-                      ),
-                    ],
-                  ),
-                  SizedBox(height: 16),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: <Widget>[
-                      Text(
-                        "Difetti uditivi",
-                        style: headlineTextStyle,
-                      ),
-                      Text(
-                        "light",
-                        style: valueTextStyle,
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ),
-          ),
-          SizedBox(height: 76),
-        ],
+                ),
+                SizedBox(height: 76),
+              ],
+            );
+          },
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () => print("Edit settings..."),
