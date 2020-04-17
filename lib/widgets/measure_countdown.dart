ba95b9b87bb9c4b7a3577b9e770da3fc9cec63d2
supercaly <djonline96@gmail.com>
2020-04-17 12:20:27 +0200

    Change in measure_countdown.dart

--- lib/widgets/measure_countdown.dart
+++ lib/widgets/measure_countdown.dart
@@ -1,16 +1,25 @@
 
 import 'dart:async';
 
-import 'package:balance_app/dialog/calibrate_device_dialog.dart';
-import 'package:balance_app/dialog/measuring_tutorial_dialog.dart';
-import 'package:balance_app/floor/measurement_database.dart';
 import 'package:flutter/material.dart';
+import 'package:balance_app/floor/measurement_database.dart';
 import 'package:balance_app/manager/preference_manager.dart';
+
+import 'package:balance_app/dialog/calibrate_device_dialog.dart';
+import 'package:balance_app/dialog/leave_confirmation_dialog.dart';
+import 'package:balance_app/dialog/measuring_tutorial_dialog.dart';
 
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:balance_app/bloc/countdown_bloc.dart';
 import 'package:provider/provider.dart';
 
+/// Widget that manage the entire measuring process
+///
+/// This widget has the purpose of managing the entirety of the
+/// posture test process; it contains an instance of [CountdownBloc]
+/// rebuilding the ui on every new state, it intercept [didPopRoute]
+/// events asking the user if he wants to abort the test and close
+/// the app.
 class MeasureCountdown extends StatefulWidget {
   @override
   State<StatefulWidget> createState() => _MeasureCountdownState();
@@ -23,39 +32,20 @@ class _MeasureCountdownState extends State<MeasureCountdown> with WidgetsBinding
   @override
   void initState() {
     super.initState();
-    _bloc = CountdownBloc.create(Provider.of<MeasurementDatabase>(context, listen: false));
+    // Create aCountdownBloc with an instance of the database
+    _bloc = CountdownBloc
+      .create(Provider.of<MeasurementDatabase>(context, listen: false));
     WidgetsBinding.instance.addObserver(this);
   }
 
   @override
   Future<bool> didPopRoute() async{
     // If we are measuring ask the user if he wants to leave
-    if (_state == CountdownState.measure) {
-      return await showDialog(
-        context: context,
-        builder: (context) => AlertDialog(
-          title: Text("Are you sure you want to leave?"),
-          content: Text("If you leave the test will fail"),
-          actions: [
-            // We don't want to leave
-            FlatButton(
-              onPressed: () {
-                Navigator.pop(context, true);
-              },
-              child: Text("No"),
-            ),
-            // Stop the test and close the app
-            FlatButton(
-              onPressed: () {
-                _bloc.add(CountdownEvents.stopMeasure);
-                Navigator.pop(context, false);
-              },
-              child: Text("Yes"),
-            ),
-          ],
-        )
+    if (_state == CountdownState.measure)
+      return await showLeaveDialog(
+        context,
+        () => _bloc.add(CountdownEvents.stopMeasure)
       );
-    }
     // If we are in pre measure stop the countdown
     if (_state == CountdownState.preMeasure)
       _bloc.add(CountdownEvents.stopPreMeasure);
@@ -86,28 +76,36 @@ class _MeasureCountdownState extends State<MeasureCountdown> with WidgetsBinding
           return Column(
             children: [
               _buildWidgetForState(context, state),
-              SizedBox(height: 30),
+              SizedBox(height: 0),
               RaisedButton(
                 onPressed: () async{
-                  // Check if we can start the test
                   if (state == CountdownState.idle) {
+                    /*
+                   * Every time the user presses the start button we need to check
+                   * two conditions:
+                   * - the device is calibrated? if not ask the user to do it
+                   * - we need to show the tutorial?
+                   */
                     bool isDeviceCalibrated = await PreferenceManager.isDeviceCalibrated;
-                    bool showTutorial = true;
+                    bool showTutorial = await PreferenceManager.showMeasuringTutorial;
                     if (!isDeviceCalibrated)
                       showCalibrateDeviceDialog(context);
                     else if (showTutorial)
-                      showTutorialDialog(context);
+                      showTutorialDialog(
+                        context,
+                        () => context.bloc<CountdownBloc>().add(CountdownEvents.startPreMeasure)
+                      );
                     else
                       context.bloc<CountdownBloc>().add(CountdownEvents.startPreMeasure);
                   }
-                  // Stop the pre measure countdown
                   else if (state == CountdownState.preMeasure)
+                    // Stop the pre measure countdown
                     context.bloc<CountdownBloc>().add(CountdownEvents.stopPreMeasure);
-                  // Stop the measurement
                   else if (state == CountdownState.measure)
+                    // Stop the measurement
                     context.bloc<CountdownBloc>().add(CountdownEvents.stopMeasure);
                 },
-                child: Text(state == CountdownState.idle ? "Start" : "Stop"),
+                child: Text(state == CountdownState.idle ? "START TEST" : "STOP TEST"),
               )
             ],
           );
@@ -128,9 +126,8 @@ class _MeasureCountdownState extends State<MeasureCountdown> with WidgetsBinding
           margin: const EdgeInsets.all(20),
           width: 180,
           height: 180,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(90),
-            color: Colors.red.shade200,
+          child: Center(
+            child: Image.asset("assets/logo.png"),
           ),
         );
     }
