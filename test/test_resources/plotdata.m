630dc8e2a98d63f3fcd2e7ddcae8f666e38386a4
supercaly <djonline96@gmail.com>
2020-05-02 11:52:58 +0200

    Change plotdata.m to produce .png images of the plots

--- test/test_resources/plotdata.m
+++ test/test_resources/plotdata.m
@@ -1,22 +1,36 @@
+%#! /bin/octave -qf
+# Plot the COGv data from the files
+
+% Extract the path to files from arguments
+path = "cogv/";
+if (length(argv()) != 0)
+  path = argv(){1};
+endif
 
 % Load all the data from files
-initialData = load("test_data.txt");
-processedData = load("dropped_data.txt");
-rotatedData = load("rotated_data.txt");
-filteredData = load("filtered_data.txt");
-downsampledData = load("downsampled_data.txt");
-detrendedData = load("detrended_data.txt");
+initialData = -1;
+if (exist(strcat(path,"test_data.txt")))
+  initialData = load(strcat(path,"test_data.txt"));
+endif
+processedData = load(strcat(path,"dropped_data.txt"));
+rotatedData = load(strcat(path,"rotated_data.txt"));
+filteredData = load(strcat(path,"filtered_data.txt"));
+downsampledData = load(strcat(path,"downsampled_data.txt"));
+detrendedData = load(strcat(path,"detrended_data.txt"));
 
 % Plot initial data
-figure("name","Original Accelerometer Data","numbertitle","off");
-hold on;
-plot(initialData(:,1,1));
-plot(initialData(:,2,1));
-plot(initialData(:,3,1));
-hold off;
+if (initialData != -1)
+  figure("name","Original Accelerometer Data","numbertitle","off");
+  hold on;
+  plot(initialData(:,1,1));
+  plot(initialData(:,2,1));
+  plot(initialData(:,3,1));
+  hold off;
+  print -dpng "initial.png"
+endif
 
 % Plot all the steps
-figure("name","","numbertitle","off");
+figure("name","Processing Steps","numbertitle","off");
 subplot(2,2,1);
 hold on;
 plot(rotatedData(:,1,1));
@@ -41,6 +55,7 @@ plot(detrendedData(:,1,1));
 plot(detrendedData(:,2,1));
 hold off;
 title("Detrend");
+print -dpng "processing.png"
 
 % Plot computed data
 figure("name","Processed COGv data","numbertitle","off");
@@ -53,3 +68,4 @@ plot(processedData(:,1,1));
 plot(processedData(:,2,1));
 hold off;
 title("COGvAP & COGvML");
+print -dpng "processed.png"
