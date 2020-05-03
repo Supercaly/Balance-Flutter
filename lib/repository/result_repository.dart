9232113e274a47daeac15bd30c3d8645bf3ad424
supercaly <djonline96@gmail.com>
2020-05-03 11:05:49 +0200

    Change result_repository.dart to return the real statokinesigram
    This commit will let result_repository.dart find the correct measurement and return the precomputed statokinesigram, if the features are not present it will compute and store them, then return

--- lib/repository/result_repository.dart
+++ lib/repository/result_repository.dart
@@ -1,8 +1,6 @@
 
-import 'dart:math';
-
 import 'package:balance_app/floor/measurement_database.dart';
-import 'package:balance_app/model/cogv_data.dart';
+import 'package:balance_app/model/measurement.dart';
 import 'package:balance_app/model/statokinesigram.dart';
 import 'package:balance_app/posture_processor/posture_processor.dart';
 
@@ -12,7 +10,11 @@ class ResultRepository {
 
   ResultRepository(this.database);
 
-  /// Computes the statokinesigram from a [measurementId]
+  /// Return a [Statokinesigram] from the stored data
+  ///
+  /// If the stored [Measurement] doesn't have any features
+  /// we compute them using [PostureProcessor], store them
+  /// and then return those.
   Future<Statokinesigram> getResult(int measurementId) async {
     if (measurementId == null)
       return Future.error(ArgumentError("measurementId must be non null"));
@@ -21,50 +23,89 @@ class ResultRepository {
     final measurement = await database.measurementDao.findMeasurementById(measurementId);
     // 2. Check if the features are present and compute them if not
     if (!measurement.hasFeatures) {
-      print("Computing the features...");
       final rawMeasurementData = await database.rawMeasurementDataDao.findAllRawMeasDataForId(measurementId);
-      await PostureProcessor.computeFromData(rawMeasurementData);
+      Statokinesigram computed = await PostureProcessor.computeFromData(measurementId, rawMeasurementData);
+      // Update the measurement with the computed features
+      database.measurementDao.updateMeasurement(
+        Measurement(
+          id: measurement.id,
+          creationDate: measurement.creationDate,
+          eyesOpen: measurement.eyesOpen,
+          swayPath: computed.swayPath,
+          meanDisplacement: computed.meanDisplacement,
+          stdDisplacement: computed.stdDisplacement,
+          minDist: computed.minDist,
+          maxDist: computed.maxDist,
+          meanFrequencyML: computed.meanFrequencyML,
+          meanFrequencyAP: computed.meanFrequencyAP,
+          frequencyPeakML: computed.frequencyPeakML,
+          frequencyPeakAP: computed.frequencyPeakAP,
+          f80ML: computed.f80ML,
+          f80AP: computed.f80AP,
+          np: computed.np,
+          meanTime: computed.meanTime,
+          stdTime: computed.stdTime,
+          meanPeaks: computed.meanPeaks,
+          stdPeaks: computed.stdPeaks,
+          meanDistance: computed.meanDistance,
+          stdDistance: computed.stdDistance,
+          grX: computed.grX,
+          grY: computed.grY,
+          grZ: computed.grZ,
+          gmX: computed.gmX,
+          gmY: computed.gmY,
+          gmZ: computed.gmZ,
+          gvX: computed.gvX,
+          gvY: computed.gvY,
+          gvZ: computed.gvZ,
+          gsX: computed.gsX,
+          gsY: computed.gsY,
+          gsZ: computed.gsZ,
+          gkX: computed.gkX,
+          gkY: computed.gkY,
+          gkZ: computed.gkZ,
+        )
+      );
+      // Store the computed CogvData
+      database.cogvDataDao.insertCogvData(computed.cogv);
+      return computed;
     }
     // 3. Return a Statokinesigram with the features
-    return Future.delayed(Duration(seconds: 4), () {
-      // TODO: 24/04/20 Return a non random Statokinesigram
-      final random = Random();
-      return Statokinesigram(
-        cogv: List.generate(50, (index) => CogvData(ap: random.nextDouble(), ml: random.nextDouble())),
-        f80AP: random.nextDouble(),
-        f80ML: random.nextDouble(),
-        frequencyPeakAP: random.nextDouble(),
-        frequencyPeakML: random.nextDouble(),
-        gkX: random.nextDouble(),
-        gkY: random.nextDouble(),
-        gkZ: random.nextDouble(),
-        gmX: random.nextDouble(),
-        gmY: random.nextDouble(),
-        gmZ: random.nextDouble(),
-        grX: random.nextDouble(),
-        grY: random.nextDouble(),
-        grZ: random.nextDouble(),
-        gsX: random.nextDouble(),
-        gsY: random.nextDouble(),
-        gsZ: random.nextDouble(),
-        gvX: random.nextDouble(),
-        gvY: random.nextDouble(),
-        gvZ: random.nextDouble(),
-        maxDist: random.nextDouble(),
-        meanDisplacement: random.nextDouble(),
-        meanDistance: random.nextDouble(),
-        meanFrequencyAP: random.nextDouble(),
-        meanFrequencyML: random.nextDouble(),
-        meanPeaks: random.nextDouble(),
-        meanTime: random.nextDouble(),
-        minDist: random.nextDouble(),
-        np: random.nextDouble(),
-        stdDisplacement: random.nextDouble(),
-        stdDistance: random.nextDouble(),
-        stdPeaks: random.nextDouble(),
-        stdTime: random.nextDouble(),
-        swayPath: random.nextDouble(),
-      );
-    });
+    return Statokinesigram(
+      cogv: await database.cogvDataDao.findAllCogvDataForId(measurementId),
+      swayPath: measurement.swayPath,
+      meanDisplacement: measurement.meanDisplacement,
+      stdDisplacement: measurement.stdDisplacement,
+      minDist: measurement.minDist,
+      maxDist: measurement.maxDist,
+      meanFrequencyAP: measurement.meanFrequencyAP,
+      meanFrequencyML: measurement.meanFrequencyML,
+      frequencyPeakAP: measurement.frequencyPeakAP,
+      frequencyPeakML: measurement.frequencyPeakML,
+      f80AP: measurement.f80AP,
+      f80ML: measurement.f80ML,
+      np: measurement.np,
+      meanTime: measurement.stdTime,
+      stdTime: measurement.stdTime,
+      meanDistance: measurement.meanDistance,
+      stdDistance: measurement.stdDistance,
+      meanPeaks: measurement.meanPeaks,
+      stdPeaks: measurement.stdPeaks,
+      grX: measurement.grX,
+      grY: measurement.grY,
+      grZ: measurement.grZ,
+      gmX: measurement.gmX,
+      gmY: measurement.gmY,
+      gmZ: measurement.gmZ,
+      gvX: measurement.gvX,
+      gvY: measurement.gvY,
+      gvZ: measurement.gvZ,
+      gsX: measurement.gsX,
+      gsY: measurement.gsY,
+      gsZ: measurement.gsZ,
+      gkX: measurement.gkX,
+      gkY: measurement.gkY,
+      gkZ: measurement.gkZ,
+    );
   }
 }
